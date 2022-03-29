640077ff22d6c23b0b0adb801d98c90c53991578
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-03-29 15:51:28 +0700

    [Bài tập] Ứng dụng chuyển đổi tiền tệ

---
+++ Introduction to Java/application/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double vnd = 23000;
+    double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Mời bạn nhập số tiền USD: ");
+        usd = scanner.nextDouble();
+        double quydoi = usd * 23000;
+        System.out.println("Giá trị VNĐ: " + quydoi);
+    }
+}

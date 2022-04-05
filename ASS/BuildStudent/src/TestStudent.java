7fc643641c4347d35549f664f00d77f424519de7
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-04-05 21:42:49 +0700

    [Bài tập] Xây dựng lớp chỉ ghi trong Java

---
+++ ASS/BuildStudent/src/TestStudent.java
@@ -0,0 +1,14 @@
+import com.company.Student;
+
+public class TestStudent {
+    Student s1 = new Student();
+        s1.setName("Ngan");
+        s1.setClasses("C02");
+
+    Student s2 = new Student();
+       s2.setName("Hang");
+       s2.setClasses("C03");
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+
+}

d5c49ad6dd2b8a84baae9e1cc1149bfdd81cf35b
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-04-06 10:04:26 +0700

    [*Thực hành] Ứng dụng đếm số lượng sinh viên thi đỗ

---
+++ Array in java/StudentsPassingTheExam/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Enter the size");
+            size = scanner.nextInt();
+            if (size > 30) {
+                System.out.println("Size should not exceed 30");
+            }
+        }while (size > 30) ;
+            array = new int[size];
+            int i = 0;
+            while (i < array.length) {
+                System.out.println("Enter a mark for student " + (i + 1) + ": ");
+                array[i] = scanner.nextInt();
+                i++;
+            }
+            int count = 0;
+            System.out.println("List pf mark: ");
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i] + "\t");
+                if (array[j] >= 5 && array[j] <= 10) {
+                    count++;
+                }
+                System.out.println("\n The number of students passing the exam is " + count);
+            }
+    }
+}
+
+
+

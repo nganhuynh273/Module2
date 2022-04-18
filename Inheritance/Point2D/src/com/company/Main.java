1d6547372a768ed00601dc85ebde5d42f87de5ed
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-04-18 09:42:02 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ Inheritance/Point2D/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+        Point2D point2D1 = new Point2D(3.4f,4.5f);
+        System.out.println(point2D1);
+
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+        Point3D point3D1 = new Point3D(2.3f, 3.4f, 4.5f );
+        System.out.println(point3D1);
+    }
+    }
+

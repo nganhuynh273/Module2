4fed32793944ae336a55b02060a18b4eb1b545e5
nganhuynh273 <huynhphuongngan273@gmail.com>
2022-04-21 15:01:14 +0700

    [Optional] [Bài tập] Chuyển đổi từ hệ thập phân sang hệ nhị phân

---
+++ DSA Stack - Queue/ConvertFromDecimalToBinary/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<Integer>();
+        System.out.println("Nhập vào số thập phân: ");
+        int num = in.nextInt();
+        while (num != 0)
+        {
+            int d = num % 2;
+            stack.push(d);
+            num /= 2;
+        }
+        System.out.print("\nGiá trị nhị phân là: ");
+        while (!(stack.isEmpty() ))
+        {
+            System.out.print(stack.pop());
+        }
+    }
+}
